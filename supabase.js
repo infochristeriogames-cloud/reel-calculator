6e092b17c3f101a41d431150b3afe01f0b3af96e
infochristeriogames-cloud <infochristeriogames@gmail.com>
2026-05-20 02:14:26 +0100

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  sb_publishable_yNwEh_kkDn3yUm9M82qA3A_kweYFzFo,
-sb_secret_xhtFcTl6E5r1z-T51QyJpw_4jMq6a84
-);
