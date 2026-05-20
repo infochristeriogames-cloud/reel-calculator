94fa3969a6cae25069d0ed52e06f761bc5d2c08b
infochristeriogames-cloud <infochristeriogames@gmail.com>
2026-05-20 02:21:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "./supabase";
+import { supabase } from "src/supabase.js";
 
 export default function App() {
   const [materials, setMaterials] = useState([]);
